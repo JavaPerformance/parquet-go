38297dc1e8558c93aab9f4c4356e45ae8cd9b9e2
Paul Anderson <paul@paul-anderson.com>
2024-03-25 15:14:57 +0100

    Trying to make s390x work

--- bloom_test.go
+++ bloom_test.go
@@ -73,9 +73,9 @@ func TestSplitBlockFilter(t *testing.T) {
 			function: func(values []deprecated.Int96) bool {
 				fmt.Println("1")
 				filter := newFilter(len(values))
-				fmt.Println("2")
+				fmt.Println("2 , %d", len(values))
 				enc.EncodeInt96(filter.Bytes(), values)
-				fmt.Println("3")
+				fmt.Println("3, %d", len(filter.Bytes()))
 				for _, v := range values {
 					fmt.Println("4")
 					if !check(filter, ValueOf(v)) {
