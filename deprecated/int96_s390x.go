dd559ce59c74f23bb456986f1d5f475b0fe3f161
Paul Anderson <paul@paul-anderson.com>
2024-03-26 11:24:37 +0100

    Trying to make s390x work

--- deprecated/int96_s390x.go
+++ deprecated/int96_s390x.go
@@ -27,11 +27,13 @@ func Int32ToInt96(value int32) (i96 Int96) {
 	printInt32BitPattern(i96[1])
 	fmt.Print(" - ")
 	printInt32BitPattern(i96[2])
+	fmt.Print("\n")
 
 	return
 }
 
 func printInt32BitPattern(n uint32) {
+	fmt.Print("printInt32BitPattern\n")
 	bits := make([]string, 32)
 
 	for i := 0; i < 32; i++ {
