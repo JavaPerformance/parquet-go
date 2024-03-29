67b05373ad56eee4b5e8d974ba3022f724b29baf
Paul Anderson <paul@paul-anderson.com>
2024-03-29 14:19:07 +0100

    Trying to make s390x work

--- int96_s390x_test.go
+++ int96_s390x_test.go
@@ -60,7 +60,7 @@ func TestInt96(t *testing.T) {
 
 }
 
-func TestInt96Len(t *testing.T) {
+func TestInt96LenX(t *testing.T) {
 	testCases := []struct {
 		i           deprecated.Int96
 		expectedLen int
@@ -80,6 +80,30 @@ func TestInt96Len(t *testing.T) {
 		deprecated.PrintInt96BitPattern(tc.i)
 		result := tc.i.Len()
 
+		//		t.Logf("PASS Len() for %v to be %d, got %d", tc.i, tc.expectedLen, result)
+		if result != tc.expectedLen {
+			t.Errorf("Expected Len() for %v to be %d, got %d", tc.i, tc.expectedLen, result)
+		}
+	}
+}
+
+func TestInt96Len(t *testing.T) {
+	testCases := []struct {
+		i           deprecated.Int96
+		expectedLen int
+	}{
+		{deprecated.Int96{0xFFFFFFFF, 0xFFFFFFFF, 1}, 65},
+		{deprecated.Int96{1 << 31, 1, 0}, 33},
+		{deprecated.Int96{0, 1 << 31, 0}, 64},
+		{deprecated.Int96{1 << 31, 0, 0}, 32},
+		{deprecated.Int96{123, 0, 0}, 7},
+		{deprecated.Int96{65535, 0, 0}, 16},
+		{deprecated.Int96{0, 0, 0}, 0},
+		{deprecated.Int96{0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF}, 96}, // Fully set
+	}
+
+	for _, tc := range testCases {
+		result := tc.i.Len()
 		//		t.Logf("PASS Len() for %v to be %d, got %d", tc.i, tc.expectedLen, result)
 		if result != tc.expectedLen {
 			t.Errorf("Expected Len() for %v to be %d, got %d", tc.i, tc.expectedLen, result)
