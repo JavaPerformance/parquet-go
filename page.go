bd724436d618ea7376059b36c0d6237e373aae81
Paul Anderson <paul@paul-anderson.com>
2024-04-02 15:46:31 +0200

    Trying to make s390x work

--- page.go
+++ page.go
@@ -539,7 +539,7 @@ func (page *booleanPage) valueAt(i int) bool {
 
 	b := ((page.bits[j] >> k) & 1) != 0
 
-	fmt.Printf("page.go booleanPage.valueAt(raw=%d i=%d, j=%d k=%d ,bool=%t)\n", uint32(int(page.offset)+i), i, j, k, b)
+	fmt.Printf("page.go booleanPage.valueAt(len=%d raw=%d i=%d, j=%d k=%d ,bool=%t)\n", len(page.bits), uint32(int(page.offset)+i), i, j, k, b)
 
 	l := int(j)
 
